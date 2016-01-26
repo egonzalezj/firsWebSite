c2de8e80c81b79d26d600d20525bc46f0501e73f
egonzalezj <eli.jvn@gmail.com>
2016-01-25 22:04:13 -0700

    Files modificated

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express();
 
+app.set("view engine", "jade");
+
 app.get("/", function(req,res) {
-	res.end("Hola Mundo");
+	res.render("index");
 });
 
 app.listen(3000);
